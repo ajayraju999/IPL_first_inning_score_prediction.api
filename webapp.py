064130561f192ab6ea4df5bbc1d1f68e626331eb
Ajay raju <51759319+ajayraju111@users.noreply.github.com>
2020-08-08 22:09:36 +0530

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, render_template, request,jsonify
+import pickle
+import numpy as np
+#import streamlit as st
+# Load the
+filename = 'Ipl_first_innings_score_prediction_model.pkl'
+model = pickle.load(open(filename, 'rb'))#opening it in the read mode
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_6 = int(request.form['runs_in_prev_6'])
+        wickets_in_prev_6 = int(request.form['wickets_in_prev_6'])
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_6, wickets_in_prev_6]
+        
+        data = np.array([temp_array])
+        my_prediction = int(model.predict(data)[0])
+              
+        return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+6)
+
+
+
+if __name__ == '__main__':
+	    app.run()
+
+
+# In[ ]:
+
+
+
+
